19451cb3b50efc6e44ce2c460587008663235c41
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-06-13 14:06:41 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,7 +6,8 @@
  * @yp: second number
  *
  */
-void swap(int* xp, int* yp)
+
+void swap(int *xp, int *yp)
 {
 int temp = *xp;
 *xp = *yp;
@@ -27,14 +28,11 @@ for (i = 0; i < size - 1; i++)
 {
 for (j = 0; j < size - i - 1; j++)
 {
-    if (array[j] > array[j + 1])
-        {
-            swap(&array[j], &array[j + 1]);
-            print_array(array, size); 
-        }
-            
-    print_array(array, size);
-    }
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
 }
-    
+print_array(array, size);
+}
+}
 }
